1be8b37584b5438e9659379c26a19fc92a09ed1c
Indy <jiazheng801@gmail.com>
2020-03-27 16:28:17 -0500

    Added models to active admin and made minor changes to validation.

---
+++ app/admin/provinces.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Province do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :abbr, :gst_rate, :pst_rate, :hst_rate
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :abbr, :gst_rate, :pst_rate, :hst_rate]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,3 +1,4 @@
 class Province < ApplicationRecord
   validates :name, :abbr, presence: true
+  validates :abbr, format: { with: /\A[A-Z]{2}\z/ }
 end
