9a1d97b82798578b4c1e41e539c31680fc85e7f6
Indy <jiazheng801@gmail.com>
2020-04-05 17:30:15 -0500

    Added filter to products page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @total_pages = Product.page(1).total_pages
+    @filtered_products = Product.where(extract_filter_param)
+    @total_pages = @filtered_products.page(1).total_pages
     @page_num = extract_page_param(params[:page])
     @products = if @page_num
-                  Product.page(@page_num)
+                  @filtered_products.page(@page_num)
                 else
-                  Product.all
+                  @filtered_products.all
                 end
     @page_num ||= 0
   end
@@ -22,5 +23,15 @@ class ProductsController < ApplicationController
     else
       false
     end
+  end
+
+  def extract_filter_param
+    sql_fragment = []
+    sql_fragment.push("date(created_at) >= date('now','-3 day')") if params.key?(:new)
+    if params.key?(:ru)
+      sql_fragment.push("(date(created_at) <> date(updated_at) AND date(updated_at) >= date('now','-3 day'))")
+    end
+    sql_fragment.push("discount_percent > 0") if params.key?(:os)
+    sql_fragment.join(" OR ")
   end
 end
--- app/javascript/components/views/Products.js
+++ app/javascript/components/views/Products.js
@@ -1,8 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { Card, CardContent, Typography, Box, Grid, CircularProgress } from "@material-ui/core"
-import { Pagination } from '@material-ui/lab'
+import { Card, CardContent, Typography, Box, Grid, CircularProgress, Chip, Zoom } from "@material-ui/core"
+import { Pagination, ToggleButton } from '@material-ui/lab'
+import { Check as CheckIcon, AddCircleOutline } from '@material-ui/icons';
+import { green } from "@material-ui/core/colors";
 import Axios from "axios"
 import ProductCard from './shared/ProductCard'
 
@@ -13,6 +15,11 @@ class Products extends React.Component {
       products: null,
       page: 1,
       total_pages: 1,
+      filters: {
+        "new": false,
+        "ru": false,
+        "os": false,
+      },
     }
   }
 
@@ -21,9 +28,14 @@ class Products extends React.Component {
   }
 
   fetchProductData = (page) => {
+    const filterUrl = []
+    Object.keys(this.state.filters).map((name, index) => {
+       if (this.state.filters[name]) filterUrl.push(name)
+    })
+
     Axios({
       method: "get",
-      url: `/api/products?page=${page}`
+      url: `/api/products?page=${page}&${filterUrl.join("&")}`
     }).then(response => {
       this.setState({
         products: response.data.products,
@@ -39,35 +51,52 @@ class Products extends React.Component {
     window.scrollTo(0, 0)
   }
 
+  filterChanged = (filterName) => (event) => {
+    const filters = this.state.filters
+    filters[filterName] = !filters[filterName]
+    this.setState({ filters: filters, page: 1, products: null })
+    this.fetchProductData(1)
+  }
+
   render () {
-    if (this.state.products == null) {
-      return (
-        <div>
-          <Box>
-            <Typography variant="h3">All Products in Galaxy Boutique</Typography>
-          </Box>
-          <CircularProgress />
-        </div>
-      )
-    } else {
-      const productsCards = this.state.products.map(product => {
+    let productsCards = <Box mt={2} ml={2}><CircularProgress /></Box>
+    if (this.state.products != null) {
+      productsCards = this.state.products.map(product => {
         return (
           <ProductCard key={product.id} product={product} />
         )
       })
-
-      return (
-        <div>
-          <Box>
-            <Typography variant="h3">All Products in Galaxy Boutique</Typography>
-          </Box>
-          { productsCards }
-          <Box mt={4}>
-            <Pagination count={this.state.total_pages} color="primary" page={this.state.page} onChange={this.pageChanged} />
-          </Box>
-        </div>
-      );
     }
+
+    if (this.state.total_pages == 0) {
+      productsCards = <Box mt={2} ml={2}><Typography variant="h6">No products match the filter</Typography></Box>
+    }
+
+    const newFilterCheck = this.state.filters["new"] ? <CheckIcon /> : <AddCircleOutline />
+    const newFilterStyle = {transition: "background-color 0.3s", backgroundColor: (this.state.filters["new"] ? green[500] : null)}
+
+    const ruFilterCheck = this.state.filters["ru"] ? <CheckIcon /> : <AddCircleOutline />
+    const ruFilterStyle = {transition: "background-color 0.3s", backgroundColor: (this.state.filters["ru"] ? green[500] : null)}
+
+    const osFilterCheck = this.state.filters["os"] ? <CheckIcon /> : <AddCircleOutline />
+    const osFilterStyle = {transition: "background-color 0.3s", backgroundColor: (this.state.filters["os"] ? green[500] : null)}
+
+    return (
+      <div>
+        <Box>
+          <Typography variant="h3">All Products in Galaxy Boutique</Typography>
+        </Box>
+        <Box mt={2} className="product-filter-buttons">
+          <Chip label="New" color="primary" clickable onClick={this.filterChanged("new")} icon={newFilterCheck} style={newFilterStyle} />
+          <Chip label="Recently Updated" color="primary" clickable onClick={this.filterChanged("ru")} icon={ruFilterCheck} style={ruFilterStyle} />
+          <Chip label="On Sale" color="primary" clickable onClick={this.filterChanged("os")} icon={osFilterCheck} style={osFilterStyle} />
+        </Box>
+        { productsCards }
+        <Box mt={4}>
+          <Pagination count={this.state.total_pages} color="primary" page={this.state.page} onChange={this.pageChanged} />
+        </Box>
+      </div>
+    );
   }
 }
 
