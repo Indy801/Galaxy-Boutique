619e6d04c627c4ca7aa261191eb22a9acf47b790
Indy <jiazheng801@gmail.com>
2020-04-07 12:22:19 -0500

    Add funtionality to product card "add to cart" button.

--- app/javascript/components/views/shared/ProductCard.js
+++ app/javascript/components/views/shared/ProductCard.js
@@ -1,13 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { Box, Card, CardContent, Grid, Typography, Button, Chip } from '@material-ui/core'
+import { Box, Card, CardContent, Grid, Typography, Button, Chip, Snackbar } from '@material-ui/core'
 import { green, blue, lightBlue, red, pink } from '@material-ui/core/colors'
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
 import { ShoppingCartOutlined, List as ListIcon } from '@material-ui/icons'
+import { Alert } from "@material-ui/lab"
 import { Link } from 'react-router-dom'
 
 class ProductCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showAddItemAlert: false,
+    }
+  }
+
+  addToCart = (id, name) => (event) => {
+    let cart = localStorage.getItem('cart') == null ? [] : JSON.parse(localStorage.getItem('cart'))
+    const item = cart.find(i => i.id == id)
+    if (item == undefined) {
+      cart.push({id: id, name: name, quantity: 1})
+    } else {
+      item.quantity += 1
+    }
+    console.log(JSON.stringify(cart))
+    localStorage.setItem('cart', JSON.stringify(cart))
+    this.setState({ showAddItemAlert: true })
+  }
+
+  onAddItemAlertClose = () => {
+    this.setState({ showAddItemAlert: false })
+  }
+
   render () {
     const product = this.props.product
     const salePrice = (product.price * (1 - product.discount_percent)).toFixed(2)
@@ -47,7 +72,7 @@ class ProductCard extends React.Component {
                   <Grid item>
                     <Grid container spacing={2}>
                       <Grid item><Button component={Link} to={`/product/${product.id}`} variant="contained" style={{backgroundColor: lightBlue[300]}} startIcon={<ListIcon/>}>See Details</Button></Grid>
-                      <Grid item><Button variant="contained" style={{backgroundColor: green[400]}} startIcon={<ShoppingCartOutlined/>}>Add to Cart</Button></Grid>
+                      <Grid item><Button variant="contained" style={{backgroundColor: green[400]}} startIcon={<ShoppingCartOutlined/>} onClick={this.addToCart(product.id, product.name)}>Add to Cart</Button></Grid>
                     </Grid>
                   </Grid>
                 </Grid>
@@ -55,6 +80,11 @@ class ProductCard extends React.Component {
             </Grid>
           </CardContent>
         </Card>
+        <Snackbar open={this.state.showAddItemAlert} autoHideDuration={7000} onClose={this.onAddItemAlertClose}>
+          <Alert elevation={6} variant="filled" onClose={this.onAddItemAlertClose} severity="success">
+            Added { product.name } to the cart.
+          </Alert>
+        </Snackbar>
       </Box>
     );
   }
