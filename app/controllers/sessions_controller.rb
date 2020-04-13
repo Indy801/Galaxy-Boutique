99b4047ef5fec4c74b8ac0e71f09b04bde2f1dd0
Indy <jiazheng801@gmail.com>
2020-04-13 00:21:20 -0500

    Added failure message to login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,15 @@ class SessionsController < Devise::SessionsController
   protect_from_forgery unless: -> { request.format.json? }
   respond_to :json
 
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#failure" }
+  end
+
+  def failure
+    resource = { error: "Incorrect email or password." }
+    render json: resource, status: :unauthorized
+  end
+
   private
 
   def respond_with(resource, _opts = {})
