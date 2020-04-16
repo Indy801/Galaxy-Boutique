0b06920b3085594ba85816a4957c93898cd7db63
Indy <jiazheng801@gmail.com>
2020-04-15 20:56:55 -0500

    Validation for models.

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ApplicationRecord
   has_many :products, through: :order_products
 
   accepts_nested_attributes_for :order_products, allow_destroy: true
+
+  validates :order_number, :subtotal, presence: true
+  validates :order_number, uniqueness: true
+  validate :validate_tax
+
+  def validate_tax
+    unless (gst && pst && !hst) || (!gst && !pst && hst)
+      errors.add(:gst, "Incorrect tax information.")
+      errors.add(:pst, "Incorrect tax information.")
+      errors.add(:hst, "Incorrect tax information.")
+    end
+  end
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -3,4 +3,13 @@ class Province < ApplicationRecord
 
   validates :name, :abbr, presence: true
   validates :abbr, format: { with: /\A[A-Z]{2}\z/ }
+  validate :validate_tax
+
+  def validate_tax
+    unless (gst_rate && pst_rate && !hst_rate) || (!gst_rate && !pst_rate && hst_rate)
+      errors.add(:gst_rate, "Incorrect tax information.")
+      errors.add(:pst_rate, "Incorrect tax information.")
+      errors.add(:hst_rate, "Incorrect tax information.")
+    end
+  end
 end
